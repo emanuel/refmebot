b8e073920d0ebd6a9ec8cbc8ec8796235eb8f4e6
Emanuel <emanuel@refme.com>
2015-10-02 16:49:43 +0100

    organization

---
+++ bin/refme_twitterbot.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+require 'refmebot/reference_reader'
+require 'refmebot'
+
+
+bot = Refmebot.new
+initial_time = Time.now
+
+# mentions to the bot so far
+puts "All mentions so far:" + bot.get_all_mentions.map {|t| [t.user.screen_name,t.text].join(' ')}.join("\n")
+
+puts  bot.find "978-0552125550 MLA"
+#looping for pooling twitter
+# while (Time.now - initial_time) < Refmebot::TOTAL_DEMO_TIME
+#   call_to_action = bot.new_mentions                                     # fetch twitter in search of new mention to the bot
+#   bot.process_new_tweets call_to_action unless call_to_action.empty?                  # process the new messages
+#   sleep(Refmebot::WAIT_BEFORE_CALLING)                                  # delay to respect twitter Rate limits
+# end
+
---
+++ lib/refmebot/reference_reader.rb
@@ -0,0 +1,19 @@
+require 'yaml'
+
+class ReferenceReader
+
+  DEFAULT_YAML = "./test_db/responses.yml"
+
+  def get_reference_map_from(file = DEFAULT_YAML)
+    results = YAML.load_file(file)
+
+    reference_map = Hash.new
+    results.each do |result|
+      id = result['id']
+      reference = result['reference']
+
+      reference_map.store(id, reference)
+    end
+    reference_map
+  end
+end
